35c4c13ec5f1849d36f92782b30a3a7f4c613b1a
wildanfatah22 <74807872+wildanfatah22@users.noreply.github.com>
2022-04-14 05:43:01 +0700

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,358 @@
+let questions = [{
+        numb: 1,
+        question: "HTML adalah singkatan dari",
+        answer: "Hyper Text Markup Language",
+        options: [
+            "Hyper Text Preprocessor",
+            "Hyper Text Markup Language",
+            "Hyper Text Multiple Language",
+            "Hyper Tool Multi Language"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Siapa yang membuat standar Web ?",
+        answer: "The World Wide Web Consortium",
+        options: [
+            "Google",
+            "The World Wide Web Consortium",
+            "Oracle",
+            "NASA"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Pilih elemen HTML yang benar untuk largest heading?  <..>",
+        answer: "h1",
+        options: [
+            "h3",
+            "heading",
+            "head",
+            "h1"
+        ]
+    },
+    {
+        numb: 4,
+        question: "Apa elemen HTML yang benar untuk menyisipkan pemisah baris / line break? <..>",
+        answer: "br",
+        options: [
+            "break",
+            "br",
+            "enter",
+            "space"
+        ]
+    },
+    {
+        numb: 5,
+        question: "Apa HTML yang benar untuk menambahkan warna latar belakang ? <..>",
+        answer: 'body style="background-color:yellow;"',
+        options: [
+            'body style="background-color:yellow;"',
+            'body style="background-color=yellow;"',
+            'body style="background:yellow;"',
+            'body "background-color:yellow;"'
+        ]
+    },
+
+    {
+        numb: 6,
+        question: "Struktur dasar HTML adalah ..",
+        answer: "head dan body",
+        options: [
+            "head dan body",
+            "head, body, dan footer",
+            "head dan tag",
+            "tag"
+        ]
+    },
+
+
+    {
+        numb: 7,
+        question: "Kegunaan Heading adalah ....",
+        answer: "membuat judul dokumen",
+        options: [
+            "menebalkan tulisan",
+            "membuat judul dokumen",
+            "memotong kalimat",
+            "memisahkan baris"
+        ]
+    },
+
+    {
+        numb: 8,
+        question: "Di bawah ini yang merupakan dokumen web adalah .....",
+        answer: "*.html",
+        options: [
+            "*.doc",
+            "*.xls",
+            "*.swf",
+            "*.html"
+        ]
+    },
+
+    {
+        numb: 9,
+        question: "Perintah untuk mengatur huruf tebal pada HTML adalah? <...>",
+        answer: "b",
+        options: [
+            "b",
+            "i",
+            "u",
+            "a"
+        ]
+    },
+
+    {
+        numb: 10,
+        question: "File gambar yang akan disisipkan ke dalam HTML menggunakan tag ...",
+        answer: 'img src=".."',
+        options: [
+            'img =".."',
+            'img src=..',
+            'img src=".."',
+            'img arc=".."'
+        ]
+    },
+
+    {
+        numb: 11,
+        question: "Bahasa khusus yang berfungsi untuk mempercantik atau memperindah tampilan web adalah...",
+        answer: "CSS",
+        options: [
+            "CSS",
+            "HTML",
+            "JavaScript",
+            "PHP"
+        ]
+    },
+
+    {
+        numb: 12,
+        question: "Salah satu jenis selektor CSS adalah class. Untuk mendeklarasikan sebuah selektor class tanda yang digunakan didepan nama class adalah",
+        answer: "titik (.)",
+        options: [
+            "titik dua (:)",
+            "bintang (*)",
+            "titik (.)",
+            "pagar (#)"
+        ]
+    },
+
+    {
+        numb: 13,
+        question: "Dalam CSS dikenal konsep box model yang menyatakan bahwa setiap elemen blok HTML memiliki area padding, margin, dan border. Urutan yang benar dari letak ketiga area tersebut dalam suatu elemen HTML dari yang terdalam adalah",
+        answer: "padding-border-margin",
+        options: [
+            "padding-margin-border",
+            "padding-border-margin",
+            "border-padding-margin",
+            "margin-padding-border"
+        ]
+    },
+
+    {
+        numb: 14,
+        question: "Untuk mengatur luas area sekeliling elemen yang berbatasan dengan elemen lain, properti yang tepat untuk digunakan adalah…",
+        answer: "margin",
+        options: [
+            "padding",
+            "border",
+            "spacing",
+            "margin"
+        ]
+    },
+
+    {
+        numb: 15,
+        question: "#para a{color:#00ff00;}      Kode CSS disamping berfungsi untuk mengatur..",
+        answer: "Warna teks link dalam ID para",
+        options: [
+            "Warna teks link dalam elemen para",
+            "Warna teks link dalam ID para",
+            "Warna teks link dalam class a",
+            "Warna teks link dalam class para"
+        ]
+    },
+
+    {
+        numb: 16,
+        question: "CSS merupakan singkatan dari?",
+        answer: "Cascading Style Sheet",
+        options: [
+            "Conversion Style Sheet",
+            "Cascading Style Sheet",
+            "Cascading Select Sheet",
+            "Cascading Style Show"
+        ]
+    },
+
+    {
+        numb: 17,
+        question: "#inggris{ Font-weight: bold;}        “Font-weight” pada kode di samping merupakan sebuah..?",
+        answer: "property",
+        options: [
+            "value",
+            "selector class",
+            "property",
+            "selector"
+        ]
+    },
+
+    {
+        numb: 18,
+        question: "Kode CSS yang benar untuk membuat semua tag p menjadi tebal…",
+        answer: "p {font-weight:bold;}",
+        options: [
+            'p {font-family:bold;}',
+            'p style="font-size:bold;"',
+            'p style="text-size:bold;"',
+            'p {font-weight:bold;}'
+        ]
+    },
+
+    {
+        numb: 19,
+        question: "Berikut ini merupakan value pada input type, kecuali…",
+        answer: "Symbol",
+        options: [
+            "Symbol",
+            "Button",
+            "Checbox",
+            "Text"
+        ]
+    },
+
+    {
+        numb: 20,
+        question: "Tipe inputan form yang berfungsi untuk menerima masukan berupa teks dari pengguna adalah…",
+        answer: "Textarea",
+        options: [
+            "Submit",
+            "Textarea",
+            "Button",
+            "File"
+        ]
+    },
+
+    {
+        numb: 21,
+        question: ".kotak{Border: solid;}   .kotak pada potongan kode diatas merupakan sebuah..",
+        answer: "selector class",
+        options: [
+            "selector class",
+            "value",
+            "property",
+            "selector"
+        ]
+    },
+
+    {
+        numb: 22,
+        question: "Perintah di javascript yang berfungsi untuk menampilkan data/informasi dalam bentuk POP UP adalalah",
+        answer: "alert()",
+        options: [
+            "write()",
+            "pop_up()",
+            "show()",
+            "alert()"
+        ]
+    },
+
+    {
+        numb: 23,
+        question: "Di bawah ini merupakan tag javascript yang dapat disisipkan pada dokumen HTML> <...>",
+        answer: "script",
+        options: [
+            "javascript",
+            "jquery",
+            "js",
+            "script"
+        ]
+    },
+
+    {
+        numb: 24,
+        question: "Di bawah adalah sintak javascript untuk mengambil elemen HTML secara spesifik pada atribut ID",
+        answer: "document.getElementById(“demo”)",
+        options: [
+            "document.getElementByName(“demo”)",
+            "document.getElementByTagName(“demo”)",
+            "document.getElementById(“demo”)",
+            "document.getElementByClass(“demo”)"
+        ]
+    },
+
+    {
+        numb: 25,
+        question: "Kita dapat menambahkan sintak javascript dalam dokumen HTML pada bagian?",
+        answer: "Bisa di dalam tag head maupun body",
+        options: [
+            "di dalam tag head",
+            "Bisa di dalam tag head maupun body",
+            "di dalam tag body",
+            "Tidak bisa keduanya"
+        ]
+    },
+
+    {
+        numb: 26,
+        question: "Berikut sintak yang benar untuk menulis kalimat Hello World",
+        answer: "document.write(“Hello Wordl”)",
+        options: [
+            "document.write(“Hello Wordl”)",
+            "document.line(“Hello Wordl”)",
+            "document.alert(“Hello Wordl”)",
+            "document.print(“Hello Wordl”)"
+        ]
+    },
+
+    {
+        numb: 27,
+        question: "Javascript memiliki beberapa tipe data seperti berikut, kecuali;",
+        answer: "list",
+        options: [
+            "list",
+            "Numerik",
+            "Boolean",
+            "String"
+        ]
+    },
+
+    {
+        numb: 28,
+        question: "Berikut cara membuat variabel yang benar di javascript",
+        answer: "var nama_variabel;",
+        options: [
+            "variable nama_variabel;",
+            "declare nama_variabel;",
+            "var nama_variabel;",
+            "nama_variabel;"
+        ]
+    },
+
+    {
+        numb: 29,
+        question: "Bagaimana cara membuat fungsi di javascript?",
+        answer: "function nama_fungsi()",
+        options: [
+            "function => nama_fungsi()",
+            "function nama_fungsi()",
+            "function [nama_fungsi]",
+            "function : nama_fungsi()"
+        ]
+    },
+
+    {
+        numb: 30,
+        question: "Bagaimana cara memanggil fungsi di javascript?",
+        answer: "nama_fungsi()",
+        options: [
+            "function nama_fungsi()",
+            "call nama_fungsi()",
+            "show nama_fungsi()",
+            "nama_fungsi()"
+        ]
+    },
+
+];
---
+++ js/script.js
@@ -0,0 +1,223 @@
+const start_btn = document.querySelector(".start_btn button");
+const info_box = document.querySelector(".info_box");
+const exit_btn = info_box.querySelector(".buttons .quit");
+const continue_btn = info_box.querySelector(".buttons .play");
+const quiz_box = document.querySelector(".quiz_box");
+const result_box = document.querySelector(".result_box");
+const option_list = document.querySelector(".option_list");
+const time_line = document.querySelector("header .time_line");
+const timeCount = document.querySelector(".timer .timer_sec");
+
+function go() {
+    window.location.assign('quiz.html');
+}
+
+function back() {
+    window.location.assign('selectquiz.html');
+}
+
+// if startQuiz button clicked
+start_btn.onclick = () => {
+    info_box.classList.add("activeInfo"); //show info box
+}
+
+// if continueQuiz button clicked
+continue_btn.onclick = () => {
+    info_box.classList.remove("activeInfo"); //hide info box
+    quiz_box.classList.add("activeQuiz"); //show quiz box
+    showQuetions(0); //calling showQestions function
+    queCounter(1); //passing 1 parameter to queCounter
+    startTimer(30); //calling startTimer function
+    startTimerLine(0); //calling startTimerLine function
+}
+
+let timeValue = 30;
+let que_count = 0;
+let que_numb = 1;
+let userScore = 0;
+let counter;
+let counterLine;
+let widthValue = 0;
+
+const play_quiz = result_box.querySelector(".buttons .play");
+const quit_quiz = result_box.querySelector(".buttons .quit");
+
+// if playQuiz button clicked
+play_quiz.onclick = () => {
+    quiz_box.classList.add("activeQuiz"); //show quiz box
+    result_box.classList.remove("activeResult"); //hide result box
+    timeValue = 30;
+    que_count = 0;
+    que_numb = 1;
+    userScore = 0;
+    widthValue = 0;
+    showQuetions(que_count); //calling showQestions function
+    queCounter(que_numb); //passing que_numb value to queCounter
+    clearInterval(counter); //clear counter
+    clearInterval(counterLine); //clear counterLine
+    startTimer(timeValue); //calling startTimer function
+    startTimerLine(widthValue); //calling startTimerLine function
+    next_btn.classList.remove("show"); //hide the next button
+}
+
+// if quitQuiz button clicked
+quit_quiz.onclick = () => {
+    window.location.reload(); //reload the current window
+}
+
+const next_btn = document.querySelector("footer .next_btn");
+const bottom_ques_counter = document.querySelector("footer .total_que");
+
+// if Next Que button clicked
+next_btn.onclick = () => {
+    if (que_count < questions.length - 1) { //if question count is less than total question length
+        que_count++; //increment the que_count value
+        que_numb++; //increment the que_numb value
+        showQuetions(que_count); //calling showQestions function
+        queCounter(que_numb); //passing que_numb value to queCounter
+        clearInterval(counter); //clear counter
+        clearInterval(counterLine); //clear counterLine
+        startTimer(timeValue); //calling startTimer function
+        startTimerLine(widthValue); //calling startTimerLine function
+        next_btn.classList.remove("show"); //hide the next button
+    } else {
+        clearInterval(counter); //clear counter
+        clearInterval(counterLine); //clear counterLine
+        showResult(); //calling showResult function
+    }
+}
+
+
+// get questions and answer with array
+function showQuetions(index) {
+    const que_text = document.querySelector(".que_text");
+    let que_tag = '<span>' + questions[index].numb + ". " + questions[index].question + '</span>';
+    let option_tag = '<div class="option"><span>' + questions[index].options[0] + '</span></div>' +
+        '<div class="option"><span>' + questions[index].options[1] + '</span></div>' +
+        '<div class="option"><span>' + questions[index].options[2] + '</span></div>' +
+        '<div class="option"><span>' + questions[index].options[3] + '</span></div>';
+    que_text.innerHTML = que_tag; //add new span tag inside que_tag
+    option_list.innerHTML = option_tag; //add new div tag inside option_tag
+
+    const option = option_list.querySelectorAll(".option");
+
+    // set onclick attribute to all available options
+    for (i = 0; i < option.length; i++) {
+        option[i].setAttribute("onclick", "optionSelected(this)");
+    }
+}
+// creating the new div tags which for icons
+let tickIconTag = '<div class="icon tick"><i class="fas fa-check"></i></div>';
+let crossIconTag = '<div class="icon cross"><i class="fas fa-times"></i></div>';
+
+//if user clicked on answer options
+function optionSelected(answer) {
+    clearInterval(counter); //clear counter
+    clearInterval(counterLine); //clear counterLine
+    let userAns = answer.textContent; //getting user selected option
+    let correcAns = questions[que_count].answer; //getting correct answer from array
+    const allOptions = option_list.children.length; //getting all option items
+
+    if (userAns == correcAns) { //if user selected option is equal to array's correct answer
+        userScore += 1; //upgrading score value with 1
+        answer.classList.add("correct");
+        answer.insertAdjacentHTML("beforeend", tickIconTag);
+        console.log("Correct Answer");
+        console.log("Your correct answers = " + userScore);
+    } else {
+        answer.classList.add("incorrect");
+        answer.insertAdjacentHTML("beforeend", crossIconTag);
+        console.log("Wrong Answer");
+
+        for (i = 0; i < allOptions; i++) {
+            if (option_list.children[i].textContent == correcAns) {
+                option_list.children[i].setAttribute("class", "option correct");
+                option_list.children[i].insertAdjacentHTML("beforeend", tickIconTag);
+                console.log("Auto selected correct answer.");
+            }
+        }
+    }
+    for (i = 0; i < allOptions; i++) {
+        option_list.children[i].classList.add("disabled"); //once user select an option then disabled all options
+    }
+    next_btn.classList.add("show"); //show the next button if user selected any option
+}
+
+function showResult() {
+    info_box.classList.remove("activeInfo"); //hide info box
+    quiz_box.classList.remove("activeQuiz"); //hide quiz box
+    result_box.classList.add("activeResult"); //show result box
+    const scoreText = result_box.querySelector(".score_text");
+
+    function updateProgressBar(progressBar, value) {
+        // value = Math.round(value);
+        progressBar.querySelector(".progress__fill").style.width = `${value}%`;
+        progressBar.querySelector(".progress__text").textContent = `${value}%`;
+    }
+
+
+
+    if (userScore > 16) {
+        let scoreTag = '<span>Selamat! 🎉, Kamu menjawab <p>' + userScore + '</p> dari <p>' + questions.length + '</p></span>';
+        scoreText.innerHTML = scoreTag; //adding new span tag inside score_Text
+    } else if (userScore > 10) {
+        let scoreTag = '<span>Kerja bagus 😎, Kamu menjawab <p>' + userScore + '</p> dari <p>' + questions.length + '</p></span>';
+        scoreText.innerHTML = scoreTag;
+    } else {
+        let scoreTag = '<span>Terus belajar 😐, Kamu menjawaby <p>' + userScore + '</p> dari <p>' + questions.length + '</p></span>';
+        scoreText.innerHTML = scoreTag;
+    }
+
+    userScore = userScore * 100 / questions.length;
+    const myProgressBar = document.querySelector(".progress");
+    /* Example */
+    updateProgressBar(myProgressBar, userScore);
+}
+
+function startTimer(time) {
+    counter = setInterval(timer, 1000);
+
+    function timer() {
+        timeCount.textContent = time; //changing the value of timeCount with time value
+        time--; //decrement the time value
+        if (time < 9) { //if timer is less than 9
+            let addZero = timeCount.textContent;
+            timeCount.textContent = "0" + addZero; //add a 0 before time value
+        }
+        if (time < 0) { //if timer is less than 0
+            clearInterval(counter); //clear counter
+            const allOptions = option_list.children.length; //getting all option items
+            let correcAns = questions[que_count].answer; //getting correct answer from array
+            for (i = 0; i < allOptions; i++) {
+                if (option_list.children[i].textContent == correcAns) { //if there is an option which is matched to an array answer
+                    option_list.children[i].setAttribute("class", "option correct"); //adding green color to matched option
+                    option_list.children[i].insertAdjacentHTML("beforeend", tickIconTag); //adding tick icon to matched option
+                    console.log("Auto selected correct answer.");
+                }
+            }
+            for (i = 0; i < allOptions; i++) {
+                option_list.children[i].classList.add("disabled"); //once user select an option then disabled all options
+            }
+            next_btn.classList.add("show"); //show the next button if user selected any option
+        }
+    }
+}
+
+function startTimerLine(time) {
+    counterLine = setInterval(timer, 29);
+
+    function timer() {
+        time += 0.5; //upgrade time value with 1
+        time_line.style.width = time + "px"; //increasing width of time_line with px by time value
+        if (time > 549) { //if time value is greater than 549
+            clearInterval(counterLine); //clear counterLine
+        }
+    }
+}
+
+
+
+function queCounter(index) {
+    let totalQueCounTag = '<span><p>' + index + '</p> dari <p>' + questions.length + '</p> Pertanyaan</span>';
+    bottom_ques_counter.innerHTML = totalQueCounTag; //adding new span tag inside bottom_ques_counter
+}
